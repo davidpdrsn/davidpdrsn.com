87108cf16afcde7f5777738f6d1be68f52971aa1
David Pedersen <david.pdrsn@gmail.com>
2013-04-14 00:48:01 +0200

    funky hourglass

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
 $(function(){
 
-  var text = "waitforit...".split('');
-
-  text.forEach(function(letter, i){
-    var className = "char-" + i,
-        markup = "<span class=\"" + className + "\">" + letter + "</span>";
-
-    $('h1').append(markup);
-  });
-
 });
